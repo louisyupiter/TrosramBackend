db234c6789587d9ede2a61da087cb3b5741cdb6d
Louis Yupiter Consolatio <consolatio123@gmail.com>
2021-05-27 15:49:50 +0700

    fix image

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongooseConnect = require("./config/db");
 const admin = require('firebase-admin')
 const router = require("./routes");
+const fileUpload = require('express-fileupload');
 global.__basedir = __dirname
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json({limit: '50mb'}));
 app.use("/uploads/image", express.static(path.join("image")));
 app.use("/uploads/video", express.static(path.join("video")));
 app.use(cors());
+app.use(fileUpload());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
