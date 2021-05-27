635b32bacd7e06e335cf2ea02b40d7250eb6c404
Louis Yupiter Consolatio <consolatio123@gmail.com>
2021-05-27 11:47:04 +0700

    fix app

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const app = express();
 
 mongooseConnect();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({limit: '50mb'}));
 app.use(cors());
-app.use('/api/v1/',router);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,6 +24,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use('/api/v1/',router);
 
 //firebase-admin
 admin.initializeApp({
