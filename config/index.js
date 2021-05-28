2a92ead650955e85a6d6944386acf2f5df28ebfe
Louis Yupiter Consolatio <consolatio123@gmail.com>
2021-05-28 07:46:31 +0700

    login storage gc

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const { Storage } = require('@google-cloud/storage')
+const path = require('path')
+const serviceKey = path.join(__dirname, './keys.json')
+
+const storage = new Storage({
+  keyFilename: process.env.GOOGLE_CREDENTIALS || serviceKey,
+  projectId: 'peak-emitter-188810',
+})
+
+// storage.getBuckets().then(x => console.log(x));
+
+module.exports = storage
