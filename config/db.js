0b4a939a023c3ff13ce693fa814106d9f4ff80be
Louis Yupiter Consolatio <consolatio123@gmail.com>
2021-06-07 10:46:50 +0700

    fix db connection

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ module.exports = () => {
   var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://localhost:27017/demoosram';
+    'mongodb://osram:5ojlJzchT4o8Tf90@osramdb-shard-00-00.wjuh4.mongodb.net:27017,osramdb-shard-00-01.wjuh4.mongodb.net:27017,osramdb-shard-00-02.wjuh4.mongodb.net:27017/waranty?ssl=true&replicaSet=atlas-1491g9-shard-0&authSource=admin&retryWrites=true&w=majority'
 
   mongoose.connect(
     uristring,
